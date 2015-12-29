9b8eae6ce6931016110ea72f2df3bac63b4c0ba9
Etchepare <etchepare.ubi@gmail.com>
2015-12-29 15:40:38 +0100

    Initial commit

---
+++ resumeBuilder.js
@@ -0,0 +1,62 @@
+var bio = {
+	"name" : "Guillaume Etchepare",
+	"role" :: "Data Scientist",
+	"contacts" : {
+		"mobile" : "06.46.46.53.15",
+		"email" : "guillaume.etche@gmail.com",
+		"location" : "Paris"
+	}
+	"welcomeMessage" : "cqfd",
+	"skills" : ["Data science","Python","Machine learning"],
+	"bioPic" ; "images/photo.jpg"
+}
+
+var education = {
+	"schools" : [
+	{
+		"name" : "INSA",
+		"city" : "Toulouse, FRA",
+		"degree" : "Engineer",
+		"major" : ["Data science"],
+		"dates" : 2015
+	},
+	{
+		"name" : "University of Leicester",
+		"city" : "Leicester, UK",
+		"degree" : "Master",
+		"major" : ["Quantitative Finance"],
+		"dates" : 2015
+	}],
+	"onlineCourses" : [
+	{
+		"title" : "Data visualization",
+		"school" : "Udacity",
+		"dates" : 2015,
+		"url" : "http://www.udacity.com/course/ud804"
+	}]
+}
+
+var work = {
+	"jobs": [
+	{
+		"employer" : "Ubisoft",
+		"title" : "Data Scientist Junior",
+		"dates" : "September 2015 - today",
+		"description" : "..."
+	},
+	{
+		"employer" : "Twenga",
+		"title" : "Business Analyst",
+		"dates" : "February 2015 - July 2015",
+		"description" : "..."
+	}]
+}
+
+var projects = {
+	"projets" : [
+	{
+		"title" : "Classification",
+		"dates" : 2015,
+		"description" : "..."
+	}]
+}
